b9412b255399304a194e1a944d22a7c2764afa82
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-01 16:57:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,10 @@ end
 
 def summon_captain_planet(array)
   planetter_calls = []
-  array.collect do 
-  # Your code here
+  array.collect do |calls|
+    planetter_calls << "#{calls.capitalize}"
+  end
+  planetter_calls
 end
 
 def long_planeteer_calls# code an argument here
