3d33c950abde172c5667ffd0d8dafe661f21f5c7
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2020-03-01 16:53:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  index = 0 
+  array.each_with_index do 
 end
 
 def summon_captain_planet# code an argument here
